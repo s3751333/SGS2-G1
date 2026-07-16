ba0903cd2da933fb875d734264b8dbdd4ad3ef21
s4051620 Mark Molnar <s4051620@rmit.edu.vn>
2026-07-16 14:14:27 +0700

    Navbar fix

--- js/navbar.js
+++ js/navbar.js
@@ -2,13 +2,13 @@ const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
-const nav_a_black = document.querySelector(".nav-a-black");
+
 menuBtn.onclick = () => {
     navbar.classList.add("show");
     menuBtn.classList.add("hide");
     body.classList.add("disabled");
-    nav_a_black.classList.add("nav-a-white")
 }
+
 cancelBtn.onclick = () => {
     body.classList.remove("disabled");
     navbar.classList.remove("show");
