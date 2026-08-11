54e81923c11e08c63862eb038b4244389bd8a3c3
Mark Molnar <s4051620@rmit.edu.vn>
2026-08-11 12:14:09 +0700

    Add live password feedback

--- js/register.js
+++ js/register.js
@@ -3,6 +3,19 @@ const formMessage = document.querySelector("#form-message");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 
+function showPasswordMatch() {
+  if (password.value === "" || confirmPassword.value === "") {
+    formMessage.textContent = "";
+  } else if (password.value === confirmPassword.value) {
+    formMessage.textContent = "Passwords match.";
+  } else {
+    formMessage.textContent = "Passwords do not match.";
+  }
+}
+
+password.addEventListener("input", showPasswordMatch);
+confirmPassword.addEventListener("input", showPasswordMatch);
+
 registerForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
