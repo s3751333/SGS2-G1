c5a734b301d500b0b26b3e0d1ecc1caa1ef6557a
Mark Molnar <s4051620@rmit.edu.vn>
2026-08-11 11:41:40 +0700

    Add sample user data

---
+++ data.js
@@ -0,0 +1,13 @@
+const users = [
+  {
+    id: 1,
+    fullName: "Mark Molnar",
+    username: "markmolnar",
+    email: "mark.molnar@booknook.test",
+    introduction: "I enjoy books and web development.",
+    role: "member",
+    status: "active",
+  },
+];
+
+module.exports = { users };
